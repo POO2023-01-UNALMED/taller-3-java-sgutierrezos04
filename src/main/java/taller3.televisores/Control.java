a351b5119aeca3dc8abf4f0ef3ca67c6e892f3b0
sgutierrezos04 <sgutierrezos@unal.edu.co>
2023-02-26 21:23:13 -0500

    3er envio

--- src/main/java/taller3.televisores/Control.java
+++ src/main/java/taller3.televisores/Control.java
@@ -22,8 +22,8 @@ public class Control
     public void volumenDown(){
         tv.volumenDown();
     }
-    public void setCanal(){
-        tv.setCanal();
+    public void setCanal(int canal){
+        tv.setCanal(canal);
     }
     public void enlazar(TV tv){
         this.tv=tv;
