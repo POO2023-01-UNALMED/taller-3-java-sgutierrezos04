70e7667e6396fcd3562cdd01eba29506bc8a38d7
sgutierrezos04 <sgutierrezos@unal.edu.co>
2023-02-26 21:27:33 -0500

    4rto envio

--- src/main/java/taller3.televisores/Marca.java
+++ src/main/java/taller3.televisores/Marca.java
@@ -9,12 +9,12 @@ public class Marca
         this.nombre=nombre;
     } 
 
-    public String getnombre()
+    public String getNombre()
     {
         return nombre;
     }
 
-    public void setnombre(String nombre)
+    public void setNombre(String nombre)
     {
         this.nombre=nombre;
     }
